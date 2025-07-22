5b94846964ecb55ede0b5af10c178499f12cac41
vastencn <vastencn@gmail.com>
2025-07-21 20:48:47 -0600

    calculus stuff
    tweaks some graphing details
    
    added more calculus
    
    up to trapezoidal integration now

--- pages_index.php
+++ pages_index.php
@@ -43,7 +43,7 @@ General flow is
   <li><b>Communication and Abstraction</b><br>
   At this point move on to building communication tools for exchanging ideas, asking questions, etc... The building blocks established in previous layers can be used directly, or as abstractions. Establish ways to abstract with metaphor, analogy, etc... We can create things akin to 'x is like y', 'x is y-ish' etc...
   <li><b>Plug-ins</b><br>
-  Plug in other universal definition from visual example. Add other methods fo defining from scratch.
+  Plug in other universal definition from visual example. Add other methods for defining from scratch.
 </ol>
 
 <?php
